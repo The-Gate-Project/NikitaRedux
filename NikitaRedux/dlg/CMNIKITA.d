d9f1c321e11e2968b5aefc5dc4c0f161634185dc
11jo <79876551+11jo@users.noreply.github.com>
2023-01-21 19:06:23 +0100

    V2.5
    - EE / EET compatibility
    - Add Handle Charset and IconV
    - Traification
    - Add component "Alternate portrait for Nikita"
    - Add Readme.md
    - Delete Backup

--- NikitaRedux/dlg/CMNIKITA.d
+++ NikitaRedux/dlg/CMNIKITA.d
@@ -1,62 +1,62 @@
 BEGIN CMNIKITA
 
 IF ~NumTimesTalkedTo(0)~ THEN BEGIN Greeting
- SAY ~<CHARNAME>? I was wondering if that was you, I haven't seen you since Baldur's Gate. How are you doing?~
-++ ~Not to sound rude but I have pressing matters at the moment.~ + GetLost
-++ ~Ah Nikita. I'm doing fine, how are you?~ + OfferJoin
-++ ~You seem familar, who are you?~ + WhoRU
-++ ~Nikita? Are you following me? If you are here to kill me, I'll take you out like I did to your Section friends.~ + Hostile
+ SAY @2000 /* <CHARNAME>? I was wondering if that was you, I haven't seen you since Baldur's Gate. How are you doing? */
+++ @2001 /* Not to sound rude but I have pressing matters at the moment. */ + GetLost
+++ @2002 /* Ah Nikita. I'm doing fine, how are you? */ + OfferJoin
+++ @2003 /* You seem familar, who are you? */ + WhoRU
+++ @2004 /* Nikita? Are you following me? If you are here to kill me, I'll take you out like I did to your Section friends. */ + Hostile
 END
 
 IF ~~ THEN BEGIN GetLost
-  SAY ~Well, don't let me hold you up.~ IF ~~ THEN EXIT
+  SAY @2005 /* Well, don't let me hold you up. */ IF ~~ THEN EXIT
 END
 
 IF ~~ THEN BEGIN OfferJoin
- SAY ~I am doing fine, thank you for asking. I've been looking for you since you left Baldur's Gate. The reason for that is I would like to join you and help you out. I owe you for destroying Section and freeing me from that evil organization. So can I join you?~
-++ ~I could use someone of your skill in my group, you may join me.~ + NikitaJoin
-++ ~I don't know. I really don't like the idea of a former assassin being with me. I'm afraid I will have to decline your offer.~ + NikitaRejected
-++ ~Right now I don't need any help. Maybe another time.~ + NikitaRejected
+ SAY @2006 /* I am doing fine, thank you for asking. I've been looking for you since you left Baldur's Gate. The reason for that is I would like to join you and help you out. I owe you for destroying Section and freeing me from that evil organization. So can I join you? */
+++ @2007 /* I could use someone of your skill in my group, you may join me. */ + NikitaJoin
+++ @2008 /* I don't know. I really don't like the idea of a former assassin being with me. I'm afraid I will have to decline your offer. */ + NikitaRejected
+++ @2009 /* Right now I don't need any help. Maybe another time. */ + NikitaRejected
 END
 
 IF ~~ THEN BEGIN WhoRU
- SAY ~My name is Nikita, I was a former operative of Section, the group that was hunting you down in Baldur's Gate. You destroyed their base in Beregost and with my help, you destroyed the Oversight branch in Baldur's Gate. I've been looking for you since then.~
-++ ~You mean to kill me is that it? Some sort of revenge I would imagine!~ + Hostile
-++ ~Ah Nikita, now I remember. I'm doing fine, how are you?~ + OfferJoin
-++ ~I still don't remember you nor the events that took place. But then again I was recently tortured for a period of time and may have had some damage done to my mind. How are you doing?~ + OfferJoin
+ SAY @2010 /* My name is Nikita, I was a former operative of Section, the group that was hunting you down in Baldur's Gate. You destroyed their base in Beregost and with my help, you destroyed the Oversight branch in Baldur's Gate. I've been looking for you since then. */
+++ @2011 /* You mean to kill me is that it? Some sort of revenge I would imagine! */ + Hostile
+++ @2012 /* Ah Nikita, now I remember. I'm doing fine, how are you? */ + OfferJoin
+++ @2013 /* I still don't remember you nor the events that took place. But then again I was recently tortured for a period of time and may have had some damage done to my mind. How are you doing? */ + OfferJoin
 END
 
 IF ~~ THEN BEGIN Hostile
- SAY ~Kill you? No, no nothing like that. The reason that I've been looking for you is because I would like to join you and help you out. I owe you for destroying Section and freeing me from that evil organization. So can I join you?~
-++ ~I could use someone of your skill in my group, you may join me.~ + NikitaJoin
-++ ~I don't know. I really don't like the idea of a former assassin being with me. I'm afraid I will have to decline your offer.~ + NikitaRejected
-++ ~Right now I don't need any help. Maybe another time.~ + NikitaRejected
+ SAY @2014 /* Kill you? No, no nothing like that. The reason that I've been looking for you is because I would like to join you and help you out. I owe you for destroying Section and freeing me from that evil organization. So can I join you? */
+++ @2007 /* I could use someone of your skill in my group, you may join me. */ + NikitaJoin
+++ @2008 /* I don't know. I really don't like the idea of a former assassin being with me. I'm afraid I will have to decline your offer. */ + NikitaRejected
+++ @2009 /* Right now I don't need any help. Maybe another time. */ + NikitaRejected
 END
 
 IF ~~ THEN BEGIN NikitaJoin
-  SAY ~Thank you for letting me join with you <CHARNAME>.~
-++ ~Good to have you with us, lets go.~ DO ~SetGlobal("NikitaJoined","LOCALS",1) SetGlobal("NikitaJoinedSpawnEnemies","GLOBAL",1) AddexperienceParty(9500)
+  SAY @2015 /* Thank you for letting me join with you <CHARNAME>. */
+++ @2016 /* Good to have you with us, lets go. */ DO ~SetGlobal("NikitaJoined","LOCALS",1) SetGlobal("NikitaJoinedSpawnEnemies","GLOBAL",1) AddexperienceParty(9500)
 JoinParty()
 ~ EXIT
 END
 
 IF ~~ THEN BEGIN NikitaRejected
-  SAY ~Well if you change your mind, I will be here waiting.~ IF ~~ THEN EXIT
+  SAY @2017 /* Well if you change your mind, I will be here waiting. */ IF ~~ THEN EXIT
 END
 
 IF ~NumTimesTalkedToGT(0)~ THEN BEGIN Return
-  SAY ~I see you have returned, can I join you?~
-++ ~Yes, you may join us Nikita.~ GOTO NikitaJoin
-++ ~No, not at the moment. Perhaps some other time.~ GOTO NikitaRejected
+  SAY @2018 /* I see you have returned, can I join you? */
+++ @2019 /* Yes, you may join us Nikita. */ GOTO NikitaJoin
+++ @2020 /* No, not at the moment. Perhaps some other time. */ GOTO NikitaRejected
 END
 
 
 BEGIN CMNIKITP
 
 IF ~Global("NikitaJoined","LOCALS",1)~ THEN BEGIN KickOut
-SAY ~You wish me to go?~
-IF ~~ THEN REPLY ~My mistake Nikita, please stay.~ DO ~JoinParty()~ EXIT
-IF ~~ THEN REPLY ~I think it would be better if we went separate ways.~ DO ~SetGlobal("NikitaJoinedSpawnEnemies","GLOBAL",0) EscapeArea()~ EXIT
+SAY @2021 /* You wish me to go? */
+IF ~~ THEN REPLY @2022 /* My mistake Nikita, please stay. */ DO ~JoinParty()~ EXIT
+IF ~~ THEN REPLY @2023 /* I think it would be better if we went separate ways. */ DO ~SetGlobal("NikitaJoinedSpawnEnemies","GLOBAL",0) EscapeArea()~ EXIT
 END
 
 
---
+++ Readme.md
@@ -0,0 +1,103 @@
+# WIP Dark Horizons WIP
+
+![Language](https://img.shields.io/static/v1?label=language&message=english%20%7C%20&color=informational)
+
+**Autor** : **CoM_Solaufein**
+
+[Link to original Mod Forum]()
+
+[Link to G3 Mod Forum](https://www.gibberlings3.net/forums/topic/23513-com-nikita-open-beta-released/)
+
+[Link to original Mod Download](http://downloads.chosenofmystra.net/bg2/)
+
+## Description:
+---------------
+
+NPC mod for Baldur's Gate 2 (BG2), Baldur's Gate Trilogy (BGT), Baldur's Gate 2 : Enhanced Edition (BG2EE) and the Enhanced Edition Trilogy (EET) :
+
+(For BG2 Throne of Bhaal must be included)
+
+Nikita is a WeiDU-based mod for Baldur's Gate II: Shadows of Amn and Throne of Bhaal. This mod adds a new encounter, new items, dialogs between Nikita and fellow NPCs and the player's character. As usual, Nikita should be compatible with most other WeiDu-based mods, and should be installed after all official patches and preferably the Baldurdash Fixpack or G3's Fixpack. Nikita requires the Throne of Bhaal expansion pack. This mod uses files and effects that ToB has. (Originally included in Enhanced Edition). 
+
+
+## Install:
+-----------
+
+Extract archive contents into your game directory
+
+    Double click SETUP-NikitaRedux.exe and follow the instructions.
+
+Though this mod is made using the WeiDU standard, and *should* be compatible with all other WeiDU-based mods, there is always the possibility for conflicts. We suggest you install this mod AFTER the official patches. If you encounter any problems installing the mod, please our forums. 
+
+Install this mod **AFTER** EET
+
+Install this mod **AFTER** BGT
+
+## Compatibility :
+------------------
+
+Nikita should be compatible with most Weidu mods, there is always a chance that incompatibilities will arise. This version works with Baldur's Gate 2, Baldur's Gate Trilogy, Baldur's Gate 2 : Enhanced Edition and the Enhanced Edition Trilogy.
+
+## FAQS :
+--------
+
+Q: What do I need to use this MOD?
+A: For BG2 vanilla Nikita requires Baldur's Gate II: Shadows of Amn and Throne of Bhaal with the latest Bioware/Interplay patches. We also strongly encourage the installation of the Baldurdash Fixpack or some other BG2 fix pack as well.
+
+Q: Are there any new quests?
+A: None.
+
+Q: Will Nikita take part in the Throne of Bhaal expansion?
+A: She can be used in ToB but she has no dialogs for banters and interjections at the moment, if ever. 
+
+
+## Credits
+-----------
+
+Many people are to be thanked for their support and contribution to this mod.
+
+First and foremost, we'd like to thank Bioware and Black Isle for supplying us with such an enjoyble PC game. We would also like to thank the Infinity Engine community as a whole for their ongoing dedication and support of the Baldur's Gate series.
+
+Big thanks go to these following individuals for their assistance with Nikita:
+
+    The creators of the La Femme Nikita television show. *Wink*
+    The friendly and knowledgeable members at the Chosen of Mystra.
+    Nikita's portrait by Enkida 
+
+
+## Version History:
+-------------------
+
+- Version 2.50
+
+    CHANGES
+	
+	EE / EET compatibility
+	Add Handle Charset and IconV
+	Traification
+	Add component "Alternate portrait for Nikita" 
+	Delete Backup
+
+- Version 2.00 Beta Release.
+
+    CHANGES
+
+    Redid the entire mod.
+
+    BUGFIXES
+
+    None. This is a beta.
+
+- Version 1.01 Release.
+
+    CHANGES
+
+    Redid the coding of her banter dialogs.
+
+    BUGFIXES
+
+    Resolved the Valen broken dialog problem.
+    For some odd reason her voice set did not work. That has been corrected.
+
+- Version 1.00 First Release. 
+
